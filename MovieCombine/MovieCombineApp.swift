256a66915002ea8771f6e4eaf80795de7b486cb1
Andhar7 <70223638+Andhar7@users.noreply.github.com>
2021-01-01 13:23:53 +0100

    Initial Commit

---
+++ MovieCombine/MovieCombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieCombineApp.swift
+//  MovieCombine
+//
+//  Created by Andrej Kling on 01.01.21.
+//
+
+import SwiftUI
+
+@main
+struct MovieCombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
